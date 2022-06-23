d4c3bde4bea0d1bc3afe0b91ac22956d90910b0e
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2022-06-24 00:20:25 +0530

    initial commit

--- student_info/models.py
+++ student_info/models.py
@@ -1,10 +1,14 @@
+import email
 from django.db import models
+from email_validator import validate_email
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
+
 class student(models.Model):
     name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=8)
     address = models.CharField(max_length=100)
     state = models.CharField(max_length=50)
